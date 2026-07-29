4e4695c73e5807eac8ce17cf0d341ea87dec62c0
Renzo Tan <renzotimtan@gmail.com>
2026-07-29 20:09:19 +1000

    chore: added comments on confusing variable names

--- src/components/Items/ViewWHModal.tsx
+++ src/components/Items/ViewWHModal.tsx
@@ -276,12 +276,14 @@ const ViewWHModal = ({
                                       "200px",
                                     )}
                               </td>
+                              {/* total_on_stock is basically now total_available quantity. Wrong variable naming */}
                               <td style={{ textAlign: "right" }}>
                                 {(
                                   warehouseItem.total_on_stock +
                                   warehouseItem.total_allocated
                                 ).toLocaleString()}
                               </td>
+                              {/* total_on_stock is basically now total_available */}
                               <td style={{ textAlign: "right" }}>
                                 {warehouseItem.total_on_stock.toLocaleString()}
                               </td>
